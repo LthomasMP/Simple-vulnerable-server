b36e01541e1deae1f9d080bd57d52f6faf76220b
LthomasMP <thomas.leroy.mp@gmail.com>
2019-12-20 23:53:36 +0100

    updated readme and clean

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Simple vulnerable server
 
-This is a tiny exploitable C server that I used for a presentation for my company.
+This is a tiny exploitable C server that I used for a talk for my company.
+The talk was about basics of reverse engineering.
--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-#include <unistd.h> 
 #include <stdio.h>
 #include <stdlib.h>l
 #include <string.h>
@@ -7,71 +6,71 @@
 #define BUFFER 64
 int main(void)
 {
-  while(1){
-    server();
-  }
+    while(1){
+        server();
+    }
 }
 
 void server(){
-  // Credentials
-  int id = 0;
-  char username[BUFFER];
-  char password[BUFFER];
+    // Credentials
+    int id = 0;
+    char username[BUFFER];
+    char password[BUFFER];
 
-  printf("%s", "mention your identity please\n");
-  gets(username);
-  printf("%s", "password:\n");
-  gets(password);
+    printf("%s", "mention your identity please\n");
+    gets(username);
+    printf("%s", "password:\n");
+    gets(password);
 
-  id = check_credentials(username, password);
+    id = check_credentials(username, password);
 
-  switch (id)
-  {
-    case 1:
-      printf("%s", "Hi la Ruche!\n");
-      break;
-    case 2:
-      printf("%s", "Hi admin!\n");
-      break;
-    default:
-      printf("%s", "Bad authentication\n");
-      break;
-  }
+    switch (id)
+    {
+        case 1:
+            printf("%s", "Hi la Ruche!\n");
+            break;
+        case 2:
+            printf("%s", "Hi admin!\n");
+            break;
+        default:
+            printf("%s", "Bad authentication\n");
+            break;
+    }
 }
 
 int check_credentials(char* username, char* password) {
-  if (strcmp(username,"la_ruche") == 0) {
-    char* ciphered = cipher_passwd(password);
-    if (strcmp(cipher_passwd(password), "#8'06;") == 0) {
-      free(ciphered);
-      return 1;
+    if (strcmp(username,"la_ruche") == 0) {
+        char* ciphered = cipher_passwd(password);
+        if (strcmp(cipher_passwd(password), "#8'06;") == 0) {
+            free(ciphered);
+            return 1;
+        }
+        else {
+            free(ciphered);
+            return -1;
+        }
+    }
+    else if (strcmp(username, "admin") == 0){
+        char* ciphered = cipher_passwd(password);
+        if (strcmp(ciphered, "&-(-spqc") == 0) {
+            free(ciphered);
+            return 2;
+        }
+        else {
+            free(ciphered);
+            return -1;
+        }
     }
     else {
-      free(ciphered);
-      return -1;
+        return -1;
     }
-  }
-  else if (strcmp(username, "admin") == 0){
-    char* ciphered = cipher_passwd(password);
-    if (strcmp(ciphered, "&-(-spqc") == 0) {
-      free(ciphered);
-      return 2;
-    }
-    else {
-      free(ciphered);
-      return -1;
-    }
-  }
-  else {
-    return -1;
-  }
 }
 
 char* cipher_passwd(char* passwd)
 {
     char* output = malloc(strlen(passwd)*sizeof(char));
     for (int i = 0; i < strlen(passwd); i++) {
-      output[i] = passwd[i] ^ 0x42;
+        output[i] = passwd[i] ^ 0x42;
     }
     return output;
 }
